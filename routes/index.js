07488fd3af1102f2867b217c97e73a1efd64f781
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-12-23 03:43:12 +0530

    mm

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router=express.Router();
 const home=require('../controllers/home');
 const urlController=require('../controllers/urlController')
 
-router.post('/',verifyToken,home.home);
+router.get('/',home.home);
 router.post('/shorten',verifyToken,urlController.shortId);
 router.get('/:shortUrl',urlController.redirectUrl);
 router.use('/user',require('./user'));
